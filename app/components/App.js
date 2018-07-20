0844f66a40c102c62c24c9a10fc4d3990dcfe195
Nicolas Botello <nbotell2@gmail.com>
2018-07-20 16:02:03 -0700

    reformat unneeded files

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import AnimeTable from "./Table";
 import EditAnime from "./EditAnime";
-import Button from "@material-ui/core/Button";
-import Demo  from './demo-table'
 class App extends React.Component {
   render() {
     return (

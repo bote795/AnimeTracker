56aba2da787661dd899a8f2bb095380f2f8a9edc
Nicolas Botello <nbotell2@gmail.com>
2018-07-24 19:14:05 -0700

    basic delete modal

---
+++ app/components/Popup/Popup.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Modal from "@material-ui/core/Modal";
+import Button from "@material-ui/core/Button";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+class Popup extends Component {
+  static propTypes = {
+    open: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    successButton: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    onSuccess: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired
+  };
+  styles = theme => ({
+    paper: {
+      position: "absolute",
+      width: theme.spacing.unit * 50,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing.unit * 4
+    }
+  });
+
+  div = props => {
+    const { title, body, onCancel, onSuccess, successButton } = this.props;
+    const CANCEL = browser.i18n.getMessage("PopupCancel");
+    return (
+      <div style={getModalStyle()} className={props.classes.paper}>
+        <Typography variant="title" id="modal-title">
+          {title}
+        </Typography>
+        <Typography variant="subheading" id="simple-modal-description">
+          {body}
+        </Typography>
+        <Button onClick={onSuccess}>{successButton}</Button>
+        <Button onClick={onCancel}>{CANCEL}</Button>
+      </div>
+    );
+  };
+  Body = withStyles(this.styles)(this.div);
+
+  render() {
+    const { open } = this.props;    
+    return (
+      <div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+        >
+          <this.Body />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Popup;
--- app/components/options/DeleteEntry.js
+++ app/components/options/DeleteEntry.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
+import SimpleModalWrapped from "../Popup/Popup";
 export default class DeleteEntry extends Component {
+  state = {
+    open: false
+  };
   delete = e => {
     e.preventDefault();
     const { id } = this.props;
@@ -9,15 +13,35 @@ export default class DeleteEntry extends Component {
       type: "delete",
       id
     };
-
+    this.handleClose();
     return this.props.edit(action);
   };
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
   render() {
+    const POPUP_DELETE_NAME = browser.i18n.getMessage("appPOPUPDELETEName");
+    const POPUP_DELETE_BODY = browser.i18n.getMessage("appPOPUPDELETEBody");
+    const POPUP_SUCCESS_BUTTON = browser.i18n.getMessage("PopupDelete");
+
+    const { open } = this.state;
     return (
       <div>
-        <IconButton onClick={this.delete} aria-label="Delete">
+        <IconButton onClick={this.handleOpen} aria-label="Delete">
           <DeleteIcon />
         </IconButton>
+        <SimpleModalWrapped
+          successButton={POPUP_SUCCESS_BUTTON}
+          onSuccess={this.delete}
+          onCancel={this.handleClose}
+          title={POPUP_DELETE_NAME}
+          body={POPUP_DELETE_BODY}
+          open={open}
+        />
       </div>
     );
   }
